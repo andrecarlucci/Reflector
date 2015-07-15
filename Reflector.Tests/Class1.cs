554bc4ee787c0c8f79a75477f8672b1d674fc95d
Andre Carlucci <andrecarlucci@gmail.com>
2015-07-14 22:00:28 -0300

    Projeto de testes

---
+++ Reflector.Tests/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Reflector.Tests {
+    public class Class1 {
+
+        [Fact]
+        public void Test() {
+            Assert.True(true);
+        }
+    }
+}
